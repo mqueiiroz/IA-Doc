d374c16242592c5f8fc11586c4d4c13ac5ef0557
mqueiiroz <matheus_queiroz_guerreiro@hotmail.com>
2026-08-21 13:04:55 -0400

    Teste de integração

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+#TESTE DE INTEGRAÇÃO, CONFIRMA QUE A APLICAÇÃO INICIA CORRETAMENTE
+from fastapi.testclient import TestClient
+
+from src.app.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
